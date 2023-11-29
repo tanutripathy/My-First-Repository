4248e61c1a5fc9e241445c5f523fdc71c3944e09
TANU TRIPATHY <tanutripathy634@gmail.com>
2023-11-29 22:29:53 +0530

    Initial Commit

--- NewMavenProject/src/test/java/stepDefinition/DropDownStepDeffi.java
+++ NewMavenProject/src/test/java/stepDefinition/DropDownStepDeffi.java
@@ -4,11 +4,17 @@ import browserControl.WebConnector;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.Assert;
+import org.openqa.selenium.interactions.Actions;
 import pageObject.DropDownPage;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
 
 public class DropDownStepDeffi extends WebConnector {
 
 DropDownPage page =new DropDownPage();
+
 
     @When("the user select {string} option from the dropdown list")
     public void the_user_select_option_from_the_dropdown_list(String selectoption) {
